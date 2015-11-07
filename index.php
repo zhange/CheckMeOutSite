be858b569b0154489b9aaf5d6f0c66186d46e942
Eric Zhang <ezhang2008@gmail.com>
2015-11-07 18:08:21 -0500

    service file doesn't work still

--- index.php
+++ index.php
@@ -20,7 +20,7 @@
     
 
     // This SQL statement selects ALL from the table 'Locations'
-    $sql = "SELECT '*' FROM [checkmeout].[Inventory]";
+    $sql = 'SELECT * FROM [checkmeout].[Inventory]';
      
     // Check if there are results
     if ($result = sqlsrv_query($conn, $tsql))
