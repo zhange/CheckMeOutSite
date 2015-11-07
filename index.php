e81c3ee375f4c182a937fc6839f7c0d80510dff1
Eric Zhang <ezhang2008@gmail.com>
2015-11-07 18:03:12 -0500

    service file doesn't work

--- index.php
+++ index.php
@@ -1,6 +1,35 @@
 <!DOCTYPE html>
 <html>
   <body>
+  <?php
+    try{
+        $serverName = "tcp:yc4lfe5q04.database.windows.net,1433";
+        $connectionOptions = array("Database"=>"checkmeout_db",
+            "Uid"=>"checkmeout_admin@yc4lfe5q04", "PWD"=>"YHack2015","Encrypt"="yes";);
+        $conn = sqlsrv_connect($serverName, $connectionOptions);
+        if($conn == false){
+            echo("NOPE");
+            die(FormatErrors(sqlsrv_errors()));
+        }
+    }
+    catch(Exception $e){
+        echo("Error!");
+    }
+    
+
+    // This SQL statement selects ALL from the table 'Locations'
+    $sql = "SELECT * FROM [checkmeout].[Inventory]";
+     
+    // Check if there are results
+    if ($result = sqlsrv_query($conn, $tsql))
+    {
+      print_r($result);
+    }
+    else{
+      echo("OH NO");
+    }
+    ?>
+
     <form action="/website/insert.php" method="POST">
       <b>Enter a new lock/item Combo:</b>
       <br>Lock Number:<br>
