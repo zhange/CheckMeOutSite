ef817ea09c8aed7d9ac23387dbeea4b89684f6ed
Eric Zhang <ezhang2008@gmail.com>
2015-11-07 13:29:04 -0500

    I made this2

--- index.php
+++ index.php
@@ -2,7 +2,33 @@
 <html>
   <body>
   HELLO
-  
+  <?php
+        echo "HELLO234";
+    ?>
+    <?php
+        echo "HELLO";
+        $serverName = "tcp:yc4lfe5q04.database.windows.net,1433";
+        $userName = 'checkmeout_admin@yc4lfe5q04';
+        $userPassword = 'YHack2015';
+        $dbName = "checkmeout_db";
+        $table = "Inventory";
+
+        $connectionOptions = array("Database"=>$dbName,
+            "Uid"=>$userName, "PWD"=>$userPassword);
+        $conn = sqlsrv_connect($serverName, $connectionOptions);
+        if($conn == false)
+            die(FormatErrors(sqlsrv_errors()));
+        else{
+            echo "HELLO WORLD";
+        }
+
+        /*$lockid = $_POST['lockID']
+        $itemid = $_POST['itemID']
+        $tsql = "INSERT checkmeout_db.Inventory (Lock_ID, Item_ID)
+        VALUES (100, 200) GO";
+        $result = sqlsrv_query($conn, $tsql);
+        print_r($result);*/ 
+    ?>
     <!--<form action="/website/insert.php" method="POST">
       <b>Enter a new lock/item Combo:</b>
       <br>Lock Number:<br>
