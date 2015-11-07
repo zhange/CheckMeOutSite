f815a374dd42df3da18f7c0c0c484e3c8a1e48e0
Eric Zhang <ezhang2008@gmail.com>
2015-11-07 11:55:35 -0500

    I made this

--- website/insert.php
+++ website/insert.php
@@ -1,34 +1,34 @@
 <?php
     
-    // function OpenConnection(){
-    //     $serverName = "tcp:yc4lfe5q04.database.windows.net,1433";
-    //     $connectionOptions = array("Database"=>"checkmeout_db",
-    //         "Uid"=>"checkmeout_admin@yc4lfe5q04", "pwd"=>"YHack2015");
-    //     $conn = sqlsrv_connect($serverName, $connectionOptions);
-    //     if($conn == false){
-    //         echo "string";
-    //         // die(FormatErrors(sqlsrv_errors()));
-    //     }
-    // }
+    function OpenConnection(){
+        $serverName = "tcp:yc4lfe5q04.database.windows.net,1433";
+        $connectionOptions = array("Database"=>"checkmeout_db",
+            "Uid"=>"checkmeout_admin@yc4lfe5q04", "pwd"=>"YHack2015");
+        $conn = sqlsrv_connect($serverName, $connectionOptions);
+        if($conn == false){
+            echo "string";
+            // die(FormatErrors(sqlsrv_errors()));
+        }
+    }
 
-    // function InsertData($lockid, $itemid){
-    //     try
-    //     {
-    //         $conn = OpenConnection();
-    //         $tsql = "INSERT checkmeout_db.Inventory (Lock_ID, Item_ID) VALUES ("$lockid", "$itemid");";);
+    function InsertData($lockid, $itemid){
+        try
+        {
+            $conn = OpenConnection();
+            $tsql = "INSERT checkmeout_db.Inventory (Lock_ID, Item_ID) VALUES ("$lockid", "$itemid");";);
 
-    //         $insertReview = sqlsrv_query($conn, $tsql);
-    //         if($insertReview == FALSE)
-    //             die(FormatErrors( sqlsrv_errors()));
-    //         echo "Product Key inserted is :";   
+            $insertReview = sqlsrv_query($conn, $tsql);
+            if($insertReview == FALSE)
+                die(FormatErrors( sqlsrv_errors()));
+            echo "Product Key inserted is :";   
 
-    //         sqlsrv_close($conn);
-    //     }
-    //     catch(Exception $e)
-    //     {
-    //         echo("Error!");
-    //     }
-    // }
+            sqlsrv_close($conn);
+        }
+        catch(Exception $e)
+        {
+            echo("Error!");
+        }
+    }
 
 
     // $conn = OpenConnection();
