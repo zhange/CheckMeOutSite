cb58d62d346081009775f3462a8a519a33fb1e00
Eric Zhang <ezhang2008@gmail.com>
2015-11-07 13:21:43 -0500

    I made this

--- index.php
+++ index.php
@@ -1,7 +1,31 @@
 <!DOCTYPE html>
 <html>
   <body>
-    <form action="/website/insert.php" method="POST">
+    <?php $serverName = "tcp:yc4lfe5q04.database.windows.net,1433";
+        $userName = 'checkmeout_admin@yc4lfe5q04';
+        $userPassword = 'YHack2015';
+        $dbName = "checkmeout_db";
+        $table = "Inventory";
+
+        $connectionInfo = array("Database"=>$dbName, "UID"=>$userName, "PWD"=>$userPassword, "MultipleActiveResultSets"=>true);
+        
+        $conn = sqlsrv_connect($serverName, $connectionOptions);
+        if($conn == false){
+            echo "HELLO";
+            die(FormatErrors(sqlsrv_errors()));
+        }
+        else{
+            echo "HELLO WORLD";
+        }
+
+        $lockid = $_POST['lockID']
+        $itemid = $_POST['itemID']
+        $tsql = "INSERT checkmeout_db.Inventory (Lock_ID, Item_ID)
+        VALUES (100, 200) GO";
+        $result = sqlsrv_query($conn, $tsql);
+        print_r($result); 
+    ?>
+    <!--<form action="/website/insert.php" method="POST">
       <b>Enter a new lock/item Combo:</b>
       <br>Lock Number:<br>
       <input type="text" name="lockID">
@@ -32,7 +56,7 @@
       <input type="number" name="itemLink">
       <br>
       <input type="submit" name="submitItem" value="Submit">
-    </form>
+    </form>-->
 
   </body>
 </html>
