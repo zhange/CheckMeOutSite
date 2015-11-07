497f92b2ecd7b9c447498e281a524c041a12b94b
Eric Zhang <ezhang2008@gmail.com>
2015-11-07 15:27:48 -0500

    renamed files

--- website/insert_item.php
+++ website/insert_item.php
@@ -1,34 +1,36 @@
 <?php
-    // $serverName = "yc4lfe5q04.database.windows.net,1433";
-    // $conn = mssql_connect($serverName, 'checkmeout_admin', 'YHack2015');
-    
-    // if($conn == false){
-    //     echo "not working";
-    //     die(FormatErrors(sqlsrv_errors()));
-    // }
-    // else{
-    //     echo("Error!");
-    // }
+    function OpenConnectionItem(){
+        $serverName = "tcp:yc4lfe5q04.database.windows.net,1433";
+        $userName = 'checkmeout_admin@yc4lfe5q04';
+        $userPassword = 'YHack2015';
+        $dbName = "checkmeout_db";
+        $table = "Items";
 
+        $connectionOptions = array("Database"=>$dbName,
+            "Uid"=>$userName, "PWD"=>$userPassword);
+        $conn = sqlsrv_connect($serverName, $connectionOptions);
+        if($conn == false){
+            die(FormatErrors(sqlsrv_errors()));
+        }
+        $itemid = $_POST["itemID"];
+        $storeid = $_POST["storeID"];
+        $itemname = $_POST["itemName"];
+        $itemdesc = $_POST["itemDescription"];
+        $itemprice = $_POST["itemPrice"];
+        $itemlink = $_POST["itemLink"]; 
 
-    // $itemID = $_POST["itemID"];
-    // $storeID = $_POST["storeID"];
-    // $itemName = $_POST["itemName"];
-    // $itemDescription = $_POST["itemDescription"];
-    // $itemPrice = $_POST["itemPrice"];
-    // $itemLink = $_POST["itemLink"];
+        $tsql = "INSERT INTO [checkmeout].[Items] VALUES ($itemid, $storeid, $itemname, $itemdesc, $itemprice, $itemlink);";
+        //Insert query
+        $insertReview = sqlsrv_query($conn, $tsql);
+    }
 
+    $storeid = $_POST["storeID"];
+    $itemid = $_POST["itemID"];
+    $itemname = $_POST["itemName"];
+    $itemdesc = $_POST["itemDescription"];
+    $itemprice = $_POST["itemPrice"];
+    $itemlink = $_POST["itemLink"];    
 
-    // if($lockid != "" && $itemid != ""){
-    //     $sql = "INSERT into Inventory (Item_ID,Store_ID,Item_Name, Item_Description, Item_Price, Picture_Link) VALUES ("$itemID","$storeID","$itemName","$itemDescription","$itemPrice","$itemLink");";
-    //     echo "done";
-    // }
+    OpenConnectionItem();
 
-    // if ($conn->multi_query($sql) === TRUE) {
-    //     echo "New records created successfully";
-    // } else {
-    //     echo "Error: " . $sql . "<br>" . $conn->error;
-    // }
-
-    // $conn->close();
 ?>
