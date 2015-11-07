62b5186d359d60dd9e147abdeba5a89706f553f7
Eric Zhang <ezhang2008@gmail.com>
2015-11-07 17:57:46 -0500

    service file

--- website/service.php
+++ website/service.php
@@ -18,21 +18,7 @@ $sql = "SELECT * FROM [checkmeout].[Inventory]";
 // Check if there are results
 if ($result = sqlsrv_query($conn, $tsql))
 {
-	// If so, then create a results array and a temporary one
-	// to hold the data
-	$resultArray = array();
-	$tempArray = array();
- 
-	// Loop through each row in the result set
-	while($row = $result->fetch_object())
-	{
-		// Add each row into our results array
-		$tempArray = $row;
-	    array_push($resultArray, $tempArray);
-	}
- 
-	// Finally, encode the array to JSON and output the results
-	echo json_encode($resultArray);
+	print_r($result);
 }
 
 ?>
