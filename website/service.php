29aa5e2ffc584da48d36c82a7eeb0a390c66460a
Eric Zhang <ezhang2008@gmail.com>
2015-11-07 17:59:39 -0500

    service file

--- website/service.php
+++ website/service.php
@@ -1,24 +1,25 @@
 <?php
- 
-	try{
-        $serverName = "tcp:yc4lfe5q04.database.windows.net,1433";
-        $connectionOptions = array("Database"=>"checkmeout_db",
-            "Uid"=>"checkmeout_admin@yc4lfe5q04", "PWD"=>"YHack2015","Encrypt"="yes";);
-        $conn = sqlsrv_connect($serverName, $connectionOptions);
-        if($conn == false)
-            die(FormatErrors(sqlsrv_errors()));
-    }
-    catch(Exception $e){
-        echo("Error!");
-    }
- 
-// This SQL statement selects ALL from the table 'Locations'
-$sql = "SELECT * FROM [checkmeout].[Inventory]";
- 
-// Check if there are results
-if ($result = sqlsrv_query($conn, $tsql))
-{
-	print_r($result);
-}
-
+	function OpenConnection(){
+		try{
+	        $serverName = "tcp:yc4lfe5q04.database.windows.net,1433";
+	        $connectionOptions = array("Database"=>"checkmeout_db",
+	            "Uid"=>"checkmeout_admin@yc4lfe5q04", "PWD"=>"YHack2015","Encrypt"="yes";);
+	        $conn = sqlsrv_connect($serverName, $connectionOptions);
+	        if($conn == false)
+	            die(FormatErrors(sqlsrv_errors()));
+	    }
+	    catch(Exception $e){
+	        echo("Error!");
+	    }
+	 
+		// This SQL statement selects ALL from the table 'Locations'
+		$sql = "SELECT * FROM [checkmeout].[Inventory]";
+		 
+		// Check if there are results
+		if ($result = sqlsrv_query($conn, $tsql))
+		{
+			print_r($result);
+		}
+	}
+	OpenConnection();
 ?>
