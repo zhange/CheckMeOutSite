cf8197e51635a6ad7fbb444a1cb8211a2ed47043
Eric Zhang <ezhang2008@gmail.com>
2015-11-07 21:18:08 -0500

    service file doesn't work crap

--- index.php
+++ index.php
@@ -17,7 +17,7 @@
       }
       
       // This SQL statement selects ALL from the table 'Locations'
-      $query = "SELECT [Lock_ID] FROM checkmeout.Inventory";
+      $query = "SELECT [Lock_ID], [Item_ID] FROM checkmeout_db.Inventory";
       $tvs = sqlsrv_query($conn, $query);
       echo $tvs;
       //Insert query
