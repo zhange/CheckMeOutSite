5aa9044880660ead717718d207b763efd72019ef
Eric Zhang <ezhang2008@gmail.com>
2015-11-07 19:37:36 -0500

    service file doesn't work mannnnn

--- index.php
+++ index.php
@@ -23,8 +23,8 @@
       if (!mssql_num_rows($query)) {
           echo 'No records found';
       } else {
-          for ($i = 0; $i < mssql_num_rows($query); ++$i) {
-              echo mssql_result($query, $i, 'Lock_ID'), PHP_EOL;
+          for ($i = 0; $i < mssql_num_rows($tsql); ++$i) {
+              echo mssql_result($tsql, $i, 'Lock_ID'), PHP_EOL;
           }
       }
     
