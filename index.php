39b1ebc3908d16bf1e5b4ad2775bea65dccfcb11
Eric Zhang <ezhang2008@gmail.com>
2015-11-07 19:36:47 -0500

    service file doesn't work mannnnn

--- index.php
+++ index.php
@@ -2,32 +2,32 @@
 <html>
   <body>
   <?php
-    $serverName = "tcp:yc4lfe5q04.database.windows.net,1433";
-    $userName = 'checkmeout_admin@yc4lfe5q04';
-    $userPassword = 'YHack2015';
-    $dbName = "checkmeout_db";
-    $table = "Inventory";
+      $serverName = "tcp:yc4lfe5q04.database.windows.net,1433";
+      $userName = 'checkmeout_admin@yc4lfe5q04';
+      $userPassword = 'YHack2015';
+      $dbName = "checkmeout_db";
+      $table = "Inventory";
 
-    $connectionOptions = array("Database"=>$dbName,
-        "Uid"=>$userName, "PWD"=>$userPassword);
-    $conn = sqlsrv_connect($serverName, $connectionOptions);
-    if($conn == false){
-        echo("OH NO");
-        die(FormatErrors(sqlsrv_errors()));
-    }
+      $connectionOptions = array("Database"=>$dbName,
+          "Uid"=>$userName, "PWD"=>$userPassword);
+      $conn = sqlsrv_connect($serverName, $connectionOptions);
+      if($conn == false){
+          echo("OH NO");
+          die(FormatErrors(sqlsrv_errors()));
+      }
+      
+      // This SQL statement selects ALL from the table 'Locations'
+      $tsql = mssql_query('SELECT [Lock_ID] FROM [Inventory]');
+      //Insert query
+
+      if (!mssql_num_rows($query)) {
+          echo 'No records found';
+      } else {
+          for ($i = 0; $i < mssql_num_rows($query); ++$i) {
+              echo mssql_result($query, $i, 'Lock_ID'), PHP_EOL;
+          }
+      }
     
-    // This SQL statement selects ALL from the table 'Locations'
-    $tsql = mssql_query('SELECT [Lock_ID] FROM [checkmeout_db].[Inventory]');
-    //Insert query
-
-    if (!mssql_num_rows($query)) {
-        echo 'No records found';
-    } else {
-        for ($i = 0; $i < mssql_num_rows($query); ++$i) {
-            echo mssql_result($query, $i, 'Lock_ID'), PHP_EOL;
-        }
-    }
-  
     ?>
 
     <form action="/website/insert.php" method="POST">
